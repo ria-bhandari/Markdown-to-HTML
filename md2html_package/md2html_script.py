fc077d632dca57c1d942f1b1c4327f55d5063daf
ria-bhandari <riabhan@gmail.com>
2023-09-24 16:49:59 -0700

    updated

--- md2html_package/md2html_script.py
+++ md2html_package/md2html_script.py
@@ -5,6 +5,10 @@ from typing import Set
 from pathlib import Path
 
 def check_location_file(file_name: str, black_list: Set[str], white_list: Set[str]) -> bool:
+    """
+    This function takes the file name and two sets: black list and white list. It checks if the file name must be included
+    based on the black and white lists. The function returns True if the file should be included. 
+    """
     if not black_list and not white_list:
         return True
     elif white_list and not black_list:  # only white list provided
@@ -15,6 +19,11 @@ def check_location_file(file_name: str, black_list: Set[str], white_list: Set[st
         return file_name not in black_list or file_name in white_list
 
 def convert_mdfile_to_html(input_file_path: str) -> str:
+    """
+    This function takes an input file path which represents the path of the markdown file. It converts
+    the markdown file to HTML using pypandoc library. The function returns the path to the generated 
+    HTML file as a string if the conversion is successful. 
+    """
     markdownfile_path = Path(input_file_path)
     if not markdownfile_path.exists() or not markdownfile_path.is_file() or not markdownfile_path.suffix == ' .md':
         return None
@@ -28,6 +37,12 @@ def convert_mdfile_to_html(input_file_path: str) -> str:
     return str(html_file_path)
 
 def get_md_files(input_repository_path: str):
+    """
+    This function takes the input repository path that represents the path to a Git repository.
+    It reads the black and white lists from files and retrieves a list of markdown files from the
+    Git repository which is then converted to HTML. If this conversion is successful, the 
+    function adds and commints the HTML files to the Git repository.
+    """
     white_list = read_list_folder_to_set('white_list_folder')
     black_list = read_list_folder_to_set('black_list_folder')
     git_files = get_git_files_from_the_repo(input_repository_path)
@@ -41,6 +56,10 @@ def get_md_files(input_repository_path: str):
                 commit_htmlfile(html_file_path)
 
 def read_list_folder_to_set(foldername: str) -> Set[str]:
+    """
+    This function takes a folder name and reads the content of the files in the specified
+    folder. It returns the contents of the files as a set of strings. 
+    """
     fileset = set()
     path_of_folder = Path(foldername)
     if path_of_folder.is_file():
@@ -49,6 +68,10 @@ def read_list_folder_to_set(foldername: str) -> Set[str]:
     return fileset
 
 def get_git_files_from_the_repo(repository_path: str):
+    """
+    This function takes a repository path which represents the path to a Git repository. It uses
+    Git commands and returns a list of file paths in the repository. 
+    """
     git_cmd = ['git', 'rev-parse', '--show-toplevel']
     output = subprocess.run(git_cmd, cwd=repository_path, stdout=subprocess.PIPE, text=True)
     if output.returncode == 0:
@@ -61,10 +84,18 @@ def get_git_files_from_the_repo(repository_path: str):
         return None
 
 def add_the_html_file_to_git(html_file_path: str):
+    """
+    This function takes an HTML file path and uses Git to add this HTML file to the staging 
+    area for the next commit. 
+    """
     git_add_cmd = ['git', 'add', html_file_path]
     subprocess.run(git_add_cmd)
 
 def commit_htmlfile(html_file_path: str):
+    """
+    This function takes an HTML file payj and commits it to the Git repository with a commit
+    message that indicates whether it is an addition or an update of the HTML file. 
+    """
     git_commit_cmd = ['git', 'commit', '-m', f'Add/Update the HTML file: {html_file_path}']
     subprocess.run(git_commit_cmd)
 
